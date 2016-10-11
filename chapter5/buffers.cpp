0d5649c4f942735e85cb5c3d48a4d69a305fddbe
John Marinelli <john.marinelli.dev@gmail.com>
2016-10-10 22:33:49 -0700

    multiple buffers WHOA

--- chapter5/buffers.cpp
+++ chapter5/buffers.cpp
@@ -8,8 +8,11 @@ namespace chapter5
 const char* vertex_glsl = 
 "#version 330 core \n"
 "layout (location = 0) in vec4 position; \n"
+"layout (location = 1) in vec3 color; \n"
+"out vec3 out_color; \n"
 "void main() { \n"
 "  gl_Position = position; \n"
+"  out_color = color; \n"
 "} \n";
 /*"layout (location = 0) in vec3 pos; \n"
 "layout (location = 1) in vec3 cols; \n"
@@ -23,9 +26,10 @@ const char* vertex_glsl =
 
 const char* fragment_glsl = 
 "#version 330 core \n"
-"out vec4 color; \n"
+"in vec3 out_color; \n"
+"out vec3 color; \n"
 "void main(void) { \n"
-"  color = vec4(1.0, 0.0, 1.0, 1.0); \n"
+"  color = out_color; \n"
 "} \n";
 
 class Buffers : public john::Application
@@ -48,6 +52,8 @@ protected:
 
 private:
   GLuint buffer;
+  GLuint buffer_2;
+  GLuint buffer_3;
   GLuint program_id;
 };
 
@@ -58,22 +64,48 @@ Buffers::Buffers() :
 {
 }
 
+struct Vertex {
+  float x, y, z, 
+    r, g, b;
+};
+
 void Buffers::startup()
 {
   john::Application::startup();
 
-  static const float data[] = {
+  static const float positions[] = {
     1.0f, 1.0f, 0.0f,
-    1.0f, -0.75f, 0.0f,
+    1.0f, -1.0f, 0.0f,
     -1.0f, -1.0f, 0.0f
   };
 
+  static const float colors[] = {
+    1.0f, 0.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    0.0f, 1.0f, 1.0f
+  };
+  /*
   glGenBuffers(1, &buffer);
-  glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
-  program_id = compile_shaders(vertex_glsl, fragment_glsl);
 
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+  */
+
+  glGenBuffers(1, &buffer);
+
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glEnableVertexAttribArray(0);
+
+  glGenBuffers(1, &buffer_2);
+
+  glBindBuffer(GL_ARRAY_BUFFER, buffer_2);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glEnableVertexAttribArray(1);
+
+  program_id = compile_shaders(vertex_glsl, fragment_glsl);
 }
 
 void Buffers::render(double current_time)
@@ -90,11 +122,24 @@ void Buffers::render(double current_time)
 
   glClearBufferfv(GL_COLOR, 0, color);
   glUseProgram(program_id);
+  /*
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  */
 
   glEnableVertexAttribArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, buffer);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+  glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer_2);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
   glDrawArrays(GL_TRIANGLES, 0, 3);
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
 }
 
 void Buffers::on_key(int key, int action)
