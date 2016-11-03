c845a89292a2c9a594bdc9be4afaf296deb04d0f
John Marinelli <john.marinelli.dev@gmail.com>
2016-11-03 08:04:25 -0700

    ch7

---
+++ chapter7/grass.cpp
@@ -0,0 +1,282 @@
+#include <math.h>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/ext.hpp>
+
+#include "glengine/application.hpp"
+#include "common/object.h"
+#include "common/sb6ktx.h"
+
+namespace chapter7
+{
+
+const char* vertex_glsl = 
+"// Vertex Shader                                                                                            \n"
+"// Graham Sellers                                                                                           \n"
+"// OpenGL SuperBible                                                                                        \n"
+"#version 410 core                                                                                           \n"
+"                                                                                                            \n"
+"// Incoming per vertex position                                                                             \n"
+"in vec4 vVertex;                                                                                            \n"
+"                                                                                                            \n"
+"// Output varyings                                                                                          \n"
+"out vec4 color;                                                                                             \n"
+"                                                                                                            \n"
+"uniform mat4 mvpMatrix;                                                                                     \n"
+"                                                                                                            \n"
+"uniform sampler1D grasspalette_texture;                                                \n"
+"uniform sampler2D length_texture;                                                      \n"
+"uniform sampler2D orientation_texture;                                                 \n"
+"uniform sampler2D grasscolor_texture;                                                  \n"
+"uniform sampler2D bend_texture;                                                        \n"
+"                                                                                                            \n"
+"int random(int seed, int iterations)                                                                        \n"
+"{                                                                                                           \n"
+"    int value = seed;                                                                                       \n"
+"    int n;                                                                                                  \n"
+"                                                                                                            \n"
+"    for (n = 0; n < iterations; n++) {                                                                      \n"
+"        value = ((value >> 7) ^ (value << 9)) * 15485863;                                                   \n"
+"    }                                                                                                       \n"
+"                                                                                                            \n"
+"    return value;                                                                                           \n"
+"}                                                                                                           \n"
+"                                                                                                            \n"
+"vec4 random_vector(int seed)                                                                                \n"
+"{                                                                                                           \n"
+"    int r = random(gl_InstanceID, 4);                                                                       \n"
+"    int g = random(r, 2);                                                                                   \n"
+"    int b = random(g, 2);                                                                                   \n"
+"    int a = random(b, 2);                                                                                   \n"
+"                                                                                                            \n"
+"    return vec4(float(r & 0x3FF) / 1024.0,                                                                  \n"
+"                float(g & 0x3FF) / 1024.0,                                                                  \n"
+"                float(b & 0x3FF) / 1024.0,                                                                  \n"
+"                float(a & 0x3FF) / 1024.0);                                                                 \n"
+"}                                                                                                           \n"
+"                                                                                                            \n"
+"mat4 construct_rotation_matrix(float angle)                                                                 \n"
+"{                                                                                                           \n"
+"    float st = sin(angle);                                                                                  \n"
+"    float ct = cos(angle);                                                                                  \n"
+"                                                                                                            \n"
+"    return mat4(vec4(ct, 0.0, st, 0.0),                                                                     \n"
+"                vec4(0.0, 1.0, 0.0, 0.0),                                                                   \n"
+"                vec4(-st, 0.0, ct, 0.0),                                                                    \n"
+"                vec4(0.0, 0.0, 0.0, 1.0));                                                                  \n"
+"}                                                                                                           \n"
+"                                                                                                            \n"
+"void main(void)                                                                                             \n"
+"{                                                                                                           \n"
+"    vec4 offset = vec4(float(gl_InstanceID >> 10) - 512.0,                                                  \n"
+"                       0.0f,                                                                                \n"
+"                       float(gl_InstanceID & 0x3FF) - 512.0,                                                \n"
+"                       0.0f);                                                                               \n"
+"    int number1 = random(gl_InstanceID, 3);                                                                 \n"
+"    int number2 = random(number1, 2);                                                                       \n"
+"    offset += vec4(float(number1 & 0xFF) / 256.0,                                                           \n"
+"                   0.0f,                                                                                    \n"
+"                   float(number2 & 0xFF) / 256.0,                                                           \n"
+"                   0.0f);                                                                                   \n"
+"    // float angle = float(random(number2, 2) & 0x3FF) / 1024.0;                                            \n"
+"                                                                                                            \n"
+"    vec2 texcoord = offset.xz / 1024.0 + vec2(0.5);                                                         \n"
+"                                                                                                            \n"
+"    // float bend_factor = float(random(number2, 7) & 0x3FF) / 1024.0;                                      \n"
+"    float bend_factor = texture(bend_texture, texcoord).r * 2.0;                                            \n"
+"    float bend_amount = cos(vVertex.y);                                                                     \n"
+"                                                                                                            \n"
+"    float angle = texture(orientation_texture, texcoord).r * 2.0 * 3.141592;                                \n"
+"    mat4 rot = construct_rotation_matrix(angle);                                                            \n"
+"    vec4 position = (rot * (vVertex + vec4(0.0, 0.0, bend_amount * bend_factor, 0.0))) + offset;            \n"
+"                                                                                                            \n"
+"    position *= vec4(1.0, texture(length_texture, texcoord).r * 0.9 + 0.3, 1.0, 1.0);                       \n"
+"                                                                                                            \n"
+"    gl_Position = mvpMatrix * position; // (rot * position);                                                \n"
+"    // color = vec4(random_vector(gl_InstanceID).xyz * vec3(0.1, 0.5, 0.1) + vec3(0.1, 0.4, 0.1), 1.0);     \n"
+"    // color = texture(orientation_texture, texcoord);                                                      \n"
+"    color = texture(grasspalette_texture, texture(grasscolor_texture, texcoord).r) +                        \n"
+"            vec4(random_vector(gl_InstanceID).xyz * vec3(0.1, 0.5, 0.1), 1.0);                              \n"
+"}                                                                                                           \n";
+
+const char* fragment_glsl = 
+"// Fragment Shader               \n"
+"// Graham Sellers                \n"
+"// OpenGL SuperBible             \n"
+"#version 410 core                \n"
+"                                 \n"
+"in vec4 color;                   \n"
+"                                 \n"
+"out vec4 output_color;           \n"
+"                                 \n"
+"void main(void)                  \n"
+"{                                \n"
+"    output_color = color;        \n"
+"}                                \n";
+
+class Grass : public john::Application
+{
+public:
+  Grass();
+
+  void startup();
+
+  void render(double current_time);
+
+  void shutdown();
+
+  void on_mouse(int button, int action);
+
+  void on_resize(int width, int height);
+
+  void handle_click();
+
+protected:
+  void on_key(int key, int action);
+
+private:
+  GLuint program_id,
+         vao,
+         grass_buffer,
+         tex_grass_color,
+         tex_grass_length,
+         tex_grass_orientation,
+         tex_grass_bend;
+
+  struct
+  {
+    GLint mvpMatrix;
+  } uniforms;
+
+  GLint mv_location,
+        proj_location;
+
+  float aspect;
+
+  glm::mat4 proj_matrix;
+};
+
+Grass::Grass() : 
+  john::Application(),
+  program_id(0),
+  vao(0),
+  grass_buffer(0),
+  tex_grass_length(0),
+  tex_grass_color(0),
+  tex_grass_bend(0),
+  tex_grass_orientation(0),
+  aspect(1268.0f / 768.0f),
+  proj_matrix(glm::perspective(50.0f, aspect, 0.1f, 1000.0f))
+{
+}
+
+void Grass::startup()
+{
+
+  static const GLfloat grass_blade[] =
+  {
+    -0.3f, 0.0f,
+     0.3f, 0.0f,
+    -0.20f, 1.0f,
+     0.1f, 1.3f,
+    -0.05f, 2.3f,
+     0.0f, 3.3f
+  };
+
+  glGenBuffers(1, &grass_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, grass_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(grass_blade), grass_blade, GL_STATIC_DRAW);
+
+  john::Application::startup();
+
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+  glEnableVertexAttribArray(0);
+
+  program_id = compile_shaders(vertex_glsl, fragment_glsl);
+
+  uniforms.mvpMatrix = glGetUniformLocation(program_id, "mvpMatrix");
+
+  glActiveTexture(GL_TEXTURE1);
+  tex_grass_length = sb6::ktx::file::load("/Local/Users/john/Documents/cpp/superbible/media/textures/grass_length.ktx");
+
+  glActiveTexture(GL_TEXTURE2);
+  tex_grass_orientation = sb6::ktx::file::load("/Local/Users/john/Documents/cpp/superbible/media/textures/grass_orientation.ktx");
+
+  glActiveTexture(GL_TEXTURE3);
+  tex_grass_color = sb6::ktx::file::load("/Local/Users/john/Documents/cpp/superbible/media/textures/grass_color.ktx");
+
+  glActiveTexture(GL_TEXTURE4);
+  tex_grass_bend = sb6::ktx::file::load("/Local/Users/john/Documents/cpp/superbible/media/textures/grass_bend.ktx");
+
+  glEnable(GL_CULL_FACE);
+  //glFrontFace(GL_CW);
+
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LEQUAL);
+
+}
+
+void Grass::render(double current_time) {
+  float t = (float) current_time * 0.02f;
+  float r = 550.0f;
+
+  static const GLfloat black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+  static const GLfloat one = 1.0f;
+  glClearBufferfv(GL_COLOR, 0, black);
+  glClearBufferfv(GL_DEPTH, 0, &one);
+
+  glm::mat4 mv_matrix = glm::lookAt(glm::vec3(sinf(t) * r, 25.0f, cosf(t) * r),
+      glm::vec3(0.0f, -50.0f, 0.0f),
+      glm::vec3(0.0, 1.0, 0.0));
+  glm::mat4 prj_matrix = glm::perspective(45.0f, (float) width / (float) height, 0.1f, 1000.0f);
+
+  glUseProgram(program_id);
+  glUniformMatrix4fv(uniforms.mvpMatrix, 1, GL_FALSE, glm::value_ptr(prj_matrix * mv_matrix));
+
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LEQUAL);
+
+  glViewport(0, 0, width, height);
+  glBindVertexArray(vao);
+  glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 6, 1024 * 1024);
+}
+
+void Grass::on_key(int key, int action)
+{
+}
+
+void Grass::on_resize(int w, int h)
+{
+  width = w;
+  height = h;
+  aspect = (float) w / (float) h;
+  proj_matrix = glm::perspective(50.0f, aspect, 0.1f, 1000.0f);
+  printf("Grass::on_resize(%d, %d)\n", width, height);
+}
+
+void Grass::on_mouse(int button, int action) 
+{
+}
+
+void Grass::handle_click() 
+{
+}
+
+void Grass::shutdown()
+{
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+}
+
+}
+
+int main(int argc, char* args[]) 
+{
+  chapter7::Grass* app = new chapter7::Grass;
+  app->run(app);
+
+  return 0;
+}
---
+++ chapter7/indexed_draw.cpp
@@ -0,0 +1,295 @@
+#include <math.h>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/ext.hpp>
+
+#include "glengine/application.hpp"
+#include "common/object.h"
+
+namespace chapter7
+{
+
+const char* vertex_glsl = 
+"#version 410 core \n"
+" \n"
+"in vec4 position; \n"
+" \n"
+"out VS_OUT \n"
+"{ \n"
+"  vec4 color; \n"
+"} vs_out; \n"
+" \n"
+"uniform mat4 mv_matrix; \n"
+"uniform mat4 proj_matrix; \n"
+" \n"
+"void main(void) { \n"
+"  gl_Position = proj_matrix * mv_matrix * position; \n"
+"  vs_out.color = position * 2.0 + vec4(0.5, 0.5, 0.5, 0.0);      \n"
+"} \n" ;
+
+const char* fragment_glsl = 
+"#version 410 core \n"
+" \n"
+"out vec4 color; \n"
+" \n"
+"in VS_OUT \n"
+"{ \n"
+"  vec4 color; \n"
+"} fs_in; \n"
+" \n"
+"void main(void) { \n"
+"  color = fs_in.color; \n"
+"} \n";
+
+class IndexedDraw : public john::Application
+{
+public:
+  IndexedDraw();
+
+  void startup();
+
+  void render(double current_time);
+
+  void shutdown();
+
+  void on_mouse(int button, int action);
+
+  void on_resize(int width, int height);
+
+  void handle_click();
+
+protected:
+  void on_key(int key, int action);
+
+private:
+  GLuint program_id,
+         vao,
+         position_buffer,
+         index_buffer;
+
+  GLint mv_location,
+        proj_location;
+
+  float aspect;
+
+  glm::mat4 proj_matrix;
+};
+
+IndexedDraw::IndexedDraw() : 
+  john::Application(),
+  program_id(0),
+  vao(0),
+  position_buffer(0),
+  index_buffer(0),
+  mv_location(0),
+  proj_location(0),
+  aspect(1268.0f / 768.0f),
+  proj_matrix(glm::perspective(50.0f, aspect, 0.1f, 1000.0f))
+{
+}
+
+void IndexedDraw::startup()
+{
+  program_id = compile_shaders(vertex_glsl, fragment_glsl);
+
+  mv_location = glGetUniformLocation(program_id, "mv_matrix");
+  proj_location = glGetUniformLocation(program_id, "proj_matrix");
+
+  printf("mv_location: %d proj_location %d\n", mv_location, proj_location);
+
+  john::Application::startup();
+
+  static const GLushort vertex_indices[] =
+  {
+      0, 1, 2,
+      2, 1, 3,
+      2, 3, 4,
+      4, 3, 5,
+      4, 5, 6,
+      6, 5, 7,
+      6, 7, 0,
+      0, 7, 1,
+      6, 0, 2,
+      2, 4, 6,
+      7, 5, 3,
+      7, 3, 1
+  };
+
+  static const GLfloat vertex_positions[] =
+  {
+      -0.25f, -0.25f, -0.25f,
+      -0.25f,  0.25f, -0.25f,
+       0.25f, -0.25f, -0.25f,
+       0.25f,  0.25f, -0.25f,
+       0.25f, -0.25f,  0.25f,
+       0.25f,  0.25f,  0.25f,
+      -0.25f, -0.25f,  0.25f,
+      -0.25f,  0.25f,  0.25f,
+  };
+
+  glGenBuffers(1, &position_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+  glBufferData(GL_ARRAY_BUFFER,
+      sizeof(vertex_positions),
+      vertex_positions,
+      GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glEnableVertexAttribArray(0);
+
+  glGenBuffers(1, &index_buffer);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+      sizeof(vertex_indices),
+      vertex_indices,
+      GL_STATIC_DRAW);
+
+  // set up vertex attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  glEnableVertexAttribArray(0);
+
+  glEnable(GL_CULL_FACE);
+  //glFrontFace(GL_CW);
+
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LEQUAL);
+
+}
+
+void IndexedDraw::render(double current_time) {
+  const GLfloat color[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+  const GLfloat one = 1.0f;
+
+  glViewport(0, 0, width, height);
+  glClearBufferfv(GL_DEPTH, 0, &one);
+  glClearBufferfv(GL_COLOR, 0, color);
+
+  glUseProgram(program_id);
+  glm::mat4 identity_matrix = glm::mat4(1.0f);
+  glm::mat4 view_matrix = glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, -100.0f));
+  glUniformMatrix4fv(proj_location, 1, GL_FALSE, glm::value_ptr(proj_matrix));
+#define LOTS_O_CUBES
+
+#ifdef LOTS_O_CUBES
+  int i;
+  for (i = 0; i < 24; ++i) {
+    float f = (float)i + (float)current_time * 0.3f;
+
+    glm::mat4 view_translation_matrix = glm::translate(
+      view_matrix,
+      glm::vec3(
+        sinf(2.1f * f) * 2.0f,
+        cosf(1.7f * f) * 2.0f,                        
+        sinf(1.3f * f) * cosf(1.5f * f) * 2.0f    
+      )
+    );
+
+    glm::mat4 rotated_vt_matrix = glm::rotate(
+      view_translation_matrix,
+      (float) current_time * 45.0f,
+      glm::vec3(0.0f, 1.0f, 0.0f)
+    );
+
+    glm::mat4 mv_matrix = glm::rotate(
+      rotated_vt_matrix,
+      (float) current_time * 21.0f, 
+      glm::vec3(1.0f, 0.0f, 0.0f)
+    );
+
+    glUniformMatrix4fv(mv_location, 1, GL_FALSE, glm::value_ptr(mv_matrix));
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, 0);
+  }
+#else
+  float f = (float)current_time * 0.3f;
+
+  glm::mat4 translation_matrix = 
+    glm::translate(
+      glm::mat4(),
+      glm::vec3(
+        sinf(2.1f * f) * 0.5f,
+        cosf(1.7f * f) * 0.5f,
+        sinf(1.3f * f) * cosf(1.5f * f) * 2.0f
+      )
+    );
+  
+  glm::mat4 rotation_matrix = 
+    glm::rotate(glm::mat4(), (float)current_time * 45.0f, glm::vec3(0.0f, 1.0f, 0.0f)) *
+    glm::rotate(glm::mat4(), (float)current_time * 81.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+
+  glm::mat4 scale_matrix = glm::scale(glm::mat4(), glm::vec3(1.0f, 1.0f, 1.0f));
+
+  glm::mat4 model_matrix = translation_matrix * rotation_matrix * scale_matrix * identity_matrix;
+
+
+  // NOTE
+  // glm supports chaining matrices for exactly this kind of situation
+  // and it is optimized to do so
+  // using glm::transform(glm::mat4()...) is just for explicit purposes :)
+  /*glm::mat4 view_translation_matrix = glm::translate(
+    view_matrix, 
+    glm::vec3(
+      sinf(2.1f * f) * 0.5f,
+      cosf(1.7f * f) * 0.5f,
+      sinf(1.3f * f) * cosf(1.5f * f) * 2.0f
+    )
+  );
+
+  glm::mat4 rotated_vt_matrix = glm::rotate(
+    view_translation_matrix, 
+    (float) current_time * 45.0f,
+    glm::vec3(0.0f, 1.0f, 0.0f)
+  );
+
+  glm::mat4 mv_matrix = glm::rotate(
+    rotated_vt_matrix,
+    (float) current_time * 81.0f,
+    glm::vec3(1.0f, 0.0f, 0.0f)
+  );*/
+
+
+  glm::mat4 mv_matrix = view_matrix * model_matrix;  
+  glUniformMatrix4fv(mv_location, 1, GL_FALSE, glm::value_ptr(mv_matrix));
+
+  glDrawArrays(GL_TRIANGLES, 0, 36);
+  glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, 0);
+#endif
+}
+
+void IndexedDraw::on_key(int key, int action)
+{
+}
+
+void IndexedDraw::on_resize(int w, int h)
+{
+  width = w;
+  height = h;
+  aspect = (float) w / (float) h;
+  proj_matrix = glm::perspective(50.0f, aspect, 0.1f, 1000.0f);
+  printf("IndexedDraw::on_resize(%d, %d)\n", width, height);
+}
+
+void IndexedDraw::on_mouse(int button, int action) 
+{
+}
+
+void IndexedDraw::handle_click() 
+{
+}
+
+void IndexedDraw::shutdown()
+{
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+}
+
+}
+
+int main(int argc, char* args[]) 
+{
+  chapter7::IndexedDraw* app = new chapter7::IndexedDraw;
+  app->run(app);
+
+  return 0;
+}
